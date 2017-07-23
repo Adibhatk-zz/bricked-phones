d49d4e73d313edea533af1bd6184ffddb6ec4834
Aditya Bhat K <adibhatk96@gmail.com>
2017-07-23 17:30:53 +0530

    updated 23-7-17

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/home.html'));
+});
+
+app.get('/signup',function(req,res){
+    res.sendFile(path.join(__dirname + '/signup.html'));
+});
+
+app.get('/review',function(req,res){
+	res.sendFile(path.join(__dirname + '/review.html'));
+});
+
+app.get('/phone.jpg',function(req,res){
+	res.sendFile(path.join(__dirname + '/phone.jpg'));
+});
+
+app.get('/wallpaper.jpg',function(req,res){
+	res.sendFile(path.join(__dirname + '/wallpaper.jpg'));
+});
+
+app.get('/phone_review',function(req,res){
+	res.sendFile(path.join(__dirname + '/phone_review.html'));
+});
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+});
